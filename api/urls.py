f484cfad7af1cf0f819b0ed9d512e07c4e8aaf2d
Gileno Cordeiro Duarte <gileno.cr.duarte@gmail.com>
2021-12-13 12:14:34 -0300

    Removendo modulo de API

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.urls import path, include
-from .views import MusicList
-
-   
-
-urlpatterns =[
-    path('api/', MusicList.as_view(), name='music'),
-]
